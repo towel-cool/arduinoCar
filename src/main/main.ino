9c6e3be52c889714f04ea8ea58b06c1d1cb7e678
sai <saihaansyed2003@gmail.com>
2024-09-16 16:49:06 -0400

    m1 moving

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,40 +1,27 @@
-#include <Servo.h>
+#include <AFMotor.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+// LEFT SIDE DRIVE
+// RED BLACK BLANK RED BLACK
 
-const int servoPos = 0;    // variable to store the servo position
-const int buttonPin = 3;
-int buttonState = 0;
-const int servoPin = 2;
+AF_DCMotor motor1(1);
+// red black go forward
+AF_DCMotor motor2(2);
 
-void setup() {
-  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
-  pinMode(buttonPin, INPUT);
-  pinMode(servoPin, OUTPUT);
-  myservo.write(0);
+
+void setup(){
+  Serial.begin(9600);
+
 }
 
-void sweepServo(int servoPos) {
-  if (servoPos = 0){
-    for (servoPos = 0; servoPos <= 180; servoPos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      myservo.write(servoPos);              // tell servo to go to position in variable 'pos'
-      delay(5);                       // waits 15ms for the servo to reach the position
-    }
-  }
-  if (servoPos = 0){
-    for (servoPos = 180; servoPos >= 0; servoPos -= 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      myservo.write(servoPos);              // tell servo to go to position in variable 'pos'
-      delay(5);                       // waits 15ms for the servo to reach the position
-    }
-  }
+void loop(){
+  motor2.setSpeed(speed(200));
+  motor2.run(FORWARD);
+  delay(2000);
+  motor2.run(RELEASE);
+  delay(2000);
 }
 
-void loop() {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-    sweepServo(servoPos);
-  }
+int  speed(int percent)
+{
+  return map(percent, 0, 200, 0, 255);
 }
