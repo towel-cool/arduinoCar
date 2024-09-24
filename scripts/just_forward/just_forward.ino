31ac4faa4b51c2fd8586f84b1e396a342df9d9d8
sai <saihaansyed2003@gmail.com>
2024-09-23 20:53:58 -0400

    just frwrd script

---
+++ scripts/just_forward/just_forward.ino
@@ -0,0 +1,29 @@
+#include <AFMotor.h>
+
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  motor1.setSpeed(speed(100));
+  motor1.run(FORWARD);
+  motor2.setSpeed(speed(100));
+  motor2.run(FORWARD);
+  motor3.setSpeed(speed(100));
+  motor3.run(FORWARD);
+  motor4.setSpeed(speed(100));
+  motor4.run(FORWARD);
+
+}
+int speed(int x){
+  return map(x, 0, 100, 0, 255);
+}
